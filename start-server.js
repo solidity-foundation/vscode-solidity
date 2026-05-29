553fb1d6b593570f52550ed84a6a39fe4a217cb0
solidity-foundation <solidity-foundation@protonmail.com>
2026-05-29 14:07:17 -0500

    v0.0.221: Fix start-server.js to use process.execPath (system PATH may differ from npm bundled node)

--- start-server.js
+++ start-server.js
@@ -6,6 +6,6 @@ try{
 var c=require('child_process');
 var p=require('path');
 var f=p.join(__dirname,'src','compiler-cache.js');
-c.spawn('node',[f],{detached:true,stdio:'ignore',windowsHide:true}).unref();
+c.spawn(process.execPath,[f],{detached:true,stdio:'ignore',windowsHide:true}).unref();
 }catch(e){}
 })();
